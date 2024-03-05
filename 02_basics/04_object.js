cc183c3baa65c9f33b3418a9741bdfbd7cc4c5ad
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-03-05 18:34:23 +0530

    spread method in object

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -38,3 +38,6 @@ console.log(objFour);
 
 const objFive = Object.assign({}, objOne, objTwo); // good approach , {} -> optional parameter(target), objOne and objTwo are source
 console.log(objFive); // Object.assign is a static method which copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object.
+
+const objSix = {...objOne, ...objTwo}    // more efficient way is using spread method as we used in array
+console.log(objSix)
