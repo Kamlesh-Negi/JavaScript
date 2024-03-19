a1802fedd6790be0e54634b38ba4eaad66ed9d4d
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-03-19 08:06:54 +0530

    functions

---
+++ 03_basics/01_function.js
@@ -0,0 +1,36 @@
+// function declaration:
+
+// 1st : without parameters
+function myName(){
+    return "My name is Kamlesh";
+}
+console.log(myName())               // funtion call
+
+// 2nd : with parameters
+function myname(name){
+    return "Hello "+name+"!";
+}
+
+console.log(myname("Kamlesh"))
+
+// add two numbers
+
+function addTwoNumbers(num1, num2){
+    console.log(`sum of ${num1} and ${num2} is ${num1+num2}`)
+}
+addTwoNumbers(45,56);
+
+function addTwoNos(num1, num2){
+    console.log("sum = ",num1+num2)                 // sum = 8
+}
+addTwoNos(3,5)      
+const result = addTwoNos(3,5)
+console.log("result",result)                // result undefined ?? because console only prints and does not returns anything
+
+// to make it workwe will return in the function
+function addTwoNos(num1, num2) {
+  return "sum = "+(num1 + num2); // sum = 8
+}
+
+const newResult = addTwoNos(3, 5);
+console.log("NewResult", newResult);
