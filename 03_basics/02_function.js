383793e2f810c37aba8699119a2686c0144d319a
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-04-14 12:10:56 +0530

    Passing object and array as parameter

--- 03_basics/02_function.js
+++ 03_basics/02_function.js
@@ -14,3 +14,33 @@ function CalculateCartPrice(... num1){
 }
 console.log(`Total price is ${CalculateCartPrice(200,400,656)}`)            // all prices will be placed in an array, we can use loops to calculate the total price
 
+// Passing Object as a Parameter
+
+const user = {
+    username : "Kamlesh",
+    rollNo : 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and roll No. is ${anyObject.rollNo}`)
+}
+//handleObject(user)              // calling the function
+
+
+// can also pass the object like this:
+handleObject ({
+    username : "Sam",
+    rollNo : 500
+})
+
+
+// Passing Arrays as a Parameter
+
+// e.g. accept and retrun the second element of array
+const myNewArray = [200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))  // here we passed the variable
+
+console.log(returnSecondValue([200,400,100,600]))       // here we directly passed the array
