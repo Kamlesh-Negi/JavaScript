4b26514a135aaa39394f8c7dd7d4cfff8bf7b636
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-06-02 21:48:54 +0530

    Arrow functions

---
+++ 03_basics/04_arrowFunctions.js
@@ -0,0 +1,7 @@
+const addTwo = (num1, num2) => { return num1 + num2 }
+console.log(addTwo(33,36))
+
+// implicit return : no need to write return as it is a single line expression
+
+const addTwoAgain = (num1, num2) => num1 + num2;
+console.log(addTwoAgain(33, 36));
