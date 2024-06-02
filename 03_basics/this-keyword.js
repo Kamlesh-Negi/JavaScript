2729f42e4bc5b6e735fff706445b016288236a70
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-06-02 21:07:19 +0530

    this keyword and working

--- 03_basics/this-keyword.js
+++ 03_basics/this-keyword.js
@@ -1,12 +1,28 @@
+//'use strict';
 const user = {
-    username : "Kamlesh",
-    price : 999,
-    welcomeMessage : function(){
-        console.log(`${this.username}, Welcome to our website`)
-        console.log(this)
-    }
-}
-user.welcomeMessage()
+  username: "Kamlesh",
+  price: 999,
+  welcomeMessage: function () {
+    console.log(`${this.username}, Welcome to our website`);
+    console.log(this);
+  },
+};
+user.welcomeMessage();
 
-user.username = "Sam"
-user.welcomeMessage()
+user.username = "Sam";
+user.welcomeMessage();
+
+// example: using 'this' inside a function
+
+function code() {
+  console.log(this);
+}
+code();
+
+// example
+
+function codeTwo(){
+    let username = "Himanshu"
+    console.log(this.username)
+}
+codeTwo()
