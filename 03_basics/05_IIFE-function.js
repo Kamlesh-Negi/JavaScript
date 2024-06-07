1d9e9ba95d5fa8006e84b2ee029052689f739bbb
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-06-07 22:27:40 +0530

    Real world example of IIFE

--- 03_basics/05_IIFE-function.js
+++ 03_basics/05_IIFE-function.js
@@ -15,3 +15,33 @@
     increaseCounter();
     increaseCounter();
 })();
+
+// Real-world Example: Imagine you have two scripts on your webpage, and both use a variable called 'count'. If you don't use IIFE, they might interfere with each other:
+/*
+//Script-1
+let count = 0;
+count++;
+console.log(count); // 1
+
+// Script-2
+ let count = 10;
+ count++;
+ console.log(count)
+
+ // This causes confusion
+ */
+
+ // with IIFE
+ // Script 1
+(function() {
+    let count = 0;
+    count++;
+    console.log("Script-1",count); // 1
+})();
+
+// Script 2
+(function() {
+    let count = 10;
+    count++;
+    console.log("Script-2",count); // 11
+})();
