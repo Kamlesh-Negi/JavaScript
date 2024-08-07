18fdc2368834edfe91c3e6de3fc96f2158b7db3a
Kamlesh-Negi <kamlesh3negi@gmail.com>
2024-08-07 21:26:27 +0530

    switch for string case

--- 04_control-flow/03-switch.js
+++ 04_control-flow/03-switch.js
@@ -28,3 +28,23 @@ switch(month) {
     default:
         console.log("Default month set to August");                
 }
+
+//example 2 for string type
+
+const monthName = "March";
+switch(monthName) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    default:
+        console.log("Default month set to August");                
+}
